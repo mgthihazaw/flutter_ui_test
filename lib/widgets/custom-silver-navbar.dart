70654e9f345fdd6f4dad26cf61bff50cfa3c1482
Your NameThihazaw <mgthihazaw777@gmail.com>
2020-07-10 16:17:27 +0630

    add collapse navbar

---
+++ lib/widgets/custom-silver-navbar.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:ui_component/widgets/fixed-header.dart';
+
+class CustomSilverNavbar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // No appbar provided to the Scaffold, only a body with a
+      // CustomScrollView.
+      body: CustomScrollView(
+        slivers: <Widget>[
+          // Add the app bar to the CustomScrollView.
+          SliverAppBar(
+            backgroundColor: Colors.purple,
+            // Provide a standard title.
+            title: Container(
+              width: 100.0,
+              child: Card(
+                child: Text('Hello'),
+              ),
+            ),
+            pinned: true,
+            // Allows the user to reveal the app bar if they begin scrolling
+            // back up the list of items.
+            floating: true,
+            // Display a placeholder widget to visualize the shrinking size.
+            snap: true,
+            // Make the initial height of the SliverAppBar larger than normal.
+            expandedHeight: 200,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.network(
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQi00iZ-bGVkTSEI10XYTrk1K6P_p2kBHRnDA&usqp=CAU",
+                fit: BoxFit.cover,
+              ),
+             
+            ),
+          ),
+          CurrySliverHeader(Colors.red,"Hello World"),
+
+          // Next, create a SliverList
+
+          SliverToBoxAdapter(
+            child: Container(
+              height: 50.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100.0, 
+                    child: Card(
+                      child: Center(child: Text('data')),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          //   SliverFixedExtentList(
+
+          //     itemExtent: 100.0,
+          //     delegate: new SliverChildBuilderDelegate(
+                
+          //         (BuildContext context, int index) {
+          //             return new Container(
+          //                 alignment: Alignment.center,
+          //                 color: Colors.lightBlue[100 * (index % 9)],
+          //                 child: new Text('list item $index'),
+          //             );
+          //         },
+          //     ),
+          // ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 100.0,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100.0,
+                    child: Card(
+                      child: Center(child: Text('DATA')),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          SliverList(
+            // Use a delegate to build items as they're scrolled on screen.
+            delegate: SliverChildBuilderDelegate(
+              // The builder function returns a ListTile with a title that
+              // displays the index of the current item.
+              (context, index) => ListTile(title: Text('Item #$index')),
+              // Builds 1000 ListTiles
+              childCount: 100,
+            ),
+          ),
+          SliverGrid(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200.0,
+              mainAxisSpacing: 10.0,
+              crossAxisSpacing: 10.0,
+              childAspectRatio: 4.0,
+            ),
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: Colors.teal[100 * (index % 9)],
+                  child: Text('grid item $index'),
+                );
+              },
+              childCount: 20,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
